78e86e109b3c01ac18293c78843e26adb74a545e
bekamax <bekamax05@gmail.com>
2024-05-13 17:39:20 +0600

    beka

---
+++ src/ui/input/Input.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./input.module.css"
+const Input = () => {
+  return (
+    <input className={styles.input} type="text" />
+  )
+}
+
+export default Input

049abf15ecc9131bbb9af3c77508b988ddc4460c
bekamax <bekamax05@gmail.com>
2024-05-13 19:07:24 +0600

    beka

--- src/widgets/navbar/navbar.tsx
+++ src/widgets/navbar/navbar.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import styles from "./nav.module.css";
-import { Link } from "react-router-dom";
-import Input from "../../ui/input/Input";
+import { Link, NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <div className={styles.navbar}>
-      <div className={styles.left}>
-        <Link to={"/home"}> vk</Link>
-        <Input />
-      </div>
-      <div className={styles.rigth}>
-        <img
-          src=" https://pp.userapi.com/60tZWMo4SmwcploUVl9XEt8ufnTTvDUmQ6Bj1g/mmv1pcj63C4.png"
-          alt=""
-        />
+      <div className={styles.navbar_container}>
+        <div className={styles.left}>
+          <NavLink to={"/home"} className={styles.logo}>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/60/VK_Full_Logo_%282021-present%29.svg/1280px-VK_Full_Logo_%282021-present%29.svg.png"
+              alt=""
+            />
+          </NavLink>
+          <div className={styles.group}>
+            <svg className={styles.icon} aria-hidden="true" viewBox="0 0 24 24">
+              <g>
+                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+              </g>
+            </svg>
+            <input
+              placeholder="Search"
+              type="search"
+              className={styles.input}
+            />
+          </div>
+        </div>
+        <div className={styles.right}>
+          <img
+            src="https://pp.userapi.com/60tZWMo4SmwcploUVl9XEt8ufnTTvDUmQ6Bj1g/mmv1pcj63C4.png"
+            alt=""
+          />
+        </div>
       </div>
     </div>
   );

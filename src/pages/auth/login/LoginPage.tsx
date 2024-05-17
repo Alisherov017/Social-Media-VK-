275a196c362126cd496e03bc1a37d3701e1daadb
bekamax <bekamax05@gmail.com>
2024-05-17 20:45:15 +0600

    beka

--- src/pages/auth/login/LoginPage.tsx
+++ src/pages/auth/login/LoginPage.tsx
@@ -1,11 +1,36 @@
-import React from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import NavbarLogin from "../../../widgets/navbar/NavbarLogin";
 import styles from "./login.module.css";
 import loginFoto from "./img/image.png";
-import { Link } from "react-router-dom";
-export const logo =
-  "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/VK_Compact_Logo_%282021-present%29.svg/2048px-VK_Compact_Logo_%282021-present%29.svg.png";
+import { Link, useNavigate } from "react-router-dom";
+import Button from "../../../ui/Button/Button";
+import { logo } from "../../../types";
+import { useAppDispatch } from "../../../helpers/Hooks";
+import { loginUser } from "../../../store/actions/user.actions";
+
 const LoginPage = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+const dispatch =useAppDispatch()
+const navigate=useNavigate()
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  }
+  console.log(user);
+
+  function handleSubmit(e: FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log("okk");
+
+    if (!user.email || !user.password) {
+      alert("Заполните все поля");
+      return;
+    }
+     dispatch(loginUser({ data: user, navigate }));
+  }
   return (
     <>
       <NavbarLogin />
@@ -16,25 +41,55 @@ const LoginPage = () => {
             Установите официальное мобильное приложение ВКонтакте и оставайтесь
             <br />в курсе новостей ваших друзей, где бы вы ни находились.
           </span>
-          <a href="https://apps.apple.com/ru/app/%D0%B2%D0%BA%D0%BE%D0%BD%D1%82%D0%B0%D0%BA%D1%82%D0%B5-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%D1%87%D0%B0%D1%82/id564177498">
+          <a
+            target="_blank"
+            href="https://apps.apple.com/ru/app/%D0%B2%D0%BA%D0%BE%D0%BD%D1%82%D0%B0%D0%BA%D1%82%D0%B5-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%D1%87%D0%B0%D1%82/id564177498"
+          >
             <img src={loginFoto} />
           </a>
           <div className={styles.buttons}>
-            <button className={styles.btn_login}>Google Play</button>
-            <button className={styles.btn_login}>RuStore</button>
-            <button className={styles.btn_login}>App Store</button>
+            <a href="https://play.google.com/store/apps/details?id=com.vkontakte.android">
+              <button className={styles.btn_login}>Google Play</button>
+            </a>
+            <a
+              target="_blank"
+              href="https://www.rustore.ru/catalog/app/com.vkontakte.android"
+            >
+              <button className={styles.btn_login}>RuStore</button>
+            </a>
+            <a
+              target="_blank"
+              href="https://apps.apple.com/ru/app/%D0%B2%D0%BA%D0%BE%D0%BD%D1%82%D0%B0%D0%BA%D1%82%D0%B5-%D1%81%D0%BE%D0%BE%D0%B1%D1%89%D0%B5%D0%BD%D0%B8%D1%8F-%D0%B2%D0%B8%D0%B4%D0%B5%D0%BE%D1%87%D0%B0%D1%82/id564177498"
+            >
+              <button className={styles.btn_login}>App Store</button>
+            </a>
           </div>
         </div>
         <div className={styles.login_right}>
-          <form className={styles.form_login}>
+          <form className={styles.form_login} onSubmit={handleSubmit}>
             <img src={logo} />
             <h2>Вход ВКонтакте</h2>
-            <input type="text" placeholder="Телефон или почта" />
-            <button>Войти</button>
+            <input
+              name="email"
+              type="email"
+              onChange={handleChange}
+              placeholder="Email"
+            />
+            <input
+              name="password"
+              type="password"
+              onChange={handleChange}
+              placeholder="Пароль"
+            />
+            <Button bg="#0077ff" color="#fff">
+              Войти
+            </Button>
           </form>
           <div className={styles.login_links}>
             <Link to={"/register"}>
-              <button>Создать аккаунт</button>
+              <Button bg="rgb(100, 170, 86)" color="white">
+                Создать аккаунт
+              </Button>
             </Link>
             <p>
               После регистрации вы получите доступ ко всем возможностям VK ID
--- src/pages/auth/registor/RegisterPage.tsx
+++ src/pages/auth/registor/RegisterPage.tsx
@@ -1,13 +1,50 @@
-import React, { FormEvent } from "react";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 import styles from "./register.module.css";
-import { logo } from "../login/LoginPage";
+
 import ArrowBackIcon from "@mui/icons-material/ArrowBack";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import NavbarLogin from "../../../widgets/navbar/NavbarLogin";
+import Button from "../../../ui/Button/Button";
+import { RegisterValues, logo } from "../../../types";
+import { useAppDispatch } from "../../../helpers/Hooks";
+import { registerUser } from "../../../store/actions/user.actions";
+
 const RegisterPage = () => {
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+
+  const [user, setUser] = useState<RegisterValues>({
+    email: "",
+    password: "",
+    password_confirm: "",
+  });
+
   function handleSubmit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
+
+    if (user.password.length <= 5) {
+      alert("Пароль должен быть больше 6 символов");
+      return;
+    }
+
+    if (!user.email || !user.password || !user.password_confirm) {
+      alert("Заполните все поля");
+      return;
+    }
+
+    if (user.password !== user.password_confirm) {
+      alert("Пароли не совпадают");
+      return;
+    }
+    console.log("успешная регистрация");
+    navigate("/");
+    dispatch(registerUser({ data: user, navigate }));
   }
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    const { name, value } = e.target;
+    setUser({ ...user, [name]: value });
+  }
+
   return (
     <>
       <NavbarLogin />
@@ -21,11 +58,27 @@ const RegisterPage = () => {
           <span>
             Ваш номер телефона будет использоваться для входа в аккаунт
           </span>
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <input type="text" />
-          <button>Регистрация</button>
+          <input
+            onChange={handleChange}
+            name="email"
+            type="email"
+            placeholder="Email"
+          />
+          <input
+            onChange={handleChange}
+            name="password"
+            type="password"
+            placeholder="Password"
+          />
+          <input
+            name="password_confirm"
+            onChange={handleChange}
+            type="password"
+            placeholder="Password confirm"
+          />
+          <Button bg="#0077ff" color="#fff">
+            Регистрация
+          </Button>
           <p>
             Нажимая «Продолжить», вы принимаете пользовательское соглашение и
             политику конфиденциальности
